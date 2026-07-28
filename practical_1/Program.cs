3fe15410d87ba7fdfbeeeb2ee48dcdcf99b2417c
princekumar132801 <princekumar132801@marwadiuniversity.ac.in>
2026-07-28 15:15:02 +0530

    Added Practical 1 folder

---
+++ practical_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace practical_1
+{
+    class Student
+    {
+        private int studentId;
+        private string studentName;
+        private string course;
+
+        // Constructor
+        public Student(int id, string name, string courseName)
+        {
+            studentId = id;
+            studentName = name;
+            course = courseName;
+        }
+
+        // Display Method
+        public void Display()
+        {
+            Console.WriteLine("\n===== Student Admission Details =====");
+            Console.WriteLine("Student ID   : " + studentId);
+            Console.WriteLine("Student Name : " + studentName);
+            Console.WriteLine("Course       : " + course);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string name, course;
+
+            Console.Write("Enter Student ID: ");
+            id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Student Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter Course: ");
+            course = Console.ReadLine();
+
+            // Object Creation
+            Student s1 = new Student(id, name, course);
+
+            // Display Details
+            s1.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
